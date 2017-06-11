951d57d04296e42597910df340116479038c85a4
vutrang155 <tranghoangphongvu@gmail.com>
2017-06-11 12:23:07 +0200

    Done the AttributeTest

--- src/model/AttributeTest.java
+++ src/model/AttributeTest.java
@@ -47,7 +47,8 @@ public class AttributeTest {
     }
     
     public void testGetValue() {
-	
+
+	this.attributeInt = new Attribute(this.name);
 	for ( int i = 0 ; i < values.length ; i++ ) {
 	    attributeInt.addValue(v);
 	}
@@ -62,15 +63,64 @@ public class AttributeTest {
     
     @Test()
     public void testSetValue() {
-
-	for ( int i = 0 ; i < values.lenght ; i++ ) {
+	
+	this.attributeInt = new Attribute(this.name);
+	for ( int i = 0 ; i < values.length ; i++ ) {
 	    attributeInt.addValue(v);
 	}
 
 	attributeInt.setValue(-1,4);
-	
+	Assert.assertFalse(attribute.getValue(-1) == 4);
 
+	attributeInt.setValue(999,4);
+	Assert.assertFalse(attribute.getValue(999) == 4);
+	    
+	attributeInt.setValue(0,4);
+	Assert.assertTrue(atribute.getValue(0) == 4);
+
+	attributeInt.setValue( values.length-1, 2);
+	Assert.assertTrue( attribute.getValue( values.length - 1 ) == 2 )
+	
     }
 
+    @Test()
+    public void testAddConstraint() {
+
+	this.attributeInt = new Attribute(this.name);
+	for ( int i = 0 ; i < values.length ; i++ ) {
+	    attributeInt.addValue(v);
+	}
+
+	this.attributeInt.addConstraint( Constraint.PRIMARYKEY );
+	Assert.assertTrue( attributeInt.getConstraint(0) == Constraint.PRIMARYKEY );
+
+	// Add existed constraint, for testing verifExistConstraints()
+	this.attributeInt.addConstraint( Constraint.PRIMARYKEY );
+	Assert.assertFalse( attributeInt.getConstraint(1) == Constraint.PRIMARYKEY );
+					 
+    }
+
+    @Test()
+    public void testAddCheck() {
+
+	this.attributeInt = new Attribute(this.name);
+	
+	this.attributeInt.addCheck(">=", 10);
+
+	// Also Test verifValues()
+	
+	this.attributeInt.addValue(5);
+	Assert.assertTrue( attributeInt.getValue(0) == 5 );
+
+	this.attributeInt.setValue(0,0);
+	Assert.assertTrue( attributeInt.getValue(0) == 0 );
+
+	this.attributeInt.setValue(0,10);
+	Assert.assertTrue( attributeInt.getValue(0) == 10 );
+
+	this.attributeInt.setValue(0,15);
+	Assert.assertFalse( attributeInt.getValue(0) == 15 );
+
+    }
 
 }
